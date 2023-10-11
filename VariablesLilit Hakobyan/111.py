cbcc171b616f328bac32d062dec891f592cae81e
Lilit <lilmankan@gmail.com>
2023-10-11 22:59:27 +0400

    PythonHomework

---
+++ VariablesLilit Hakobyan/111.py
@@ -0,0 +1,29 @@
+#a = 10
+#a == 10:
+    # print("Hello")
+    # break
+# b = ()
+# for num in range(1, 20, 2):
+#     print(num)
+#
+# num_list = (1, 2, 3, 4, 5, 6)
+# itr = iter(num_list)
+# print(next(itr))
+# print(next(itr))
+# print(next(itr))
+# print(next(itr))
+# print(next(itr))
+# print(next(itr))
+# for yntacik in range(1,10,):
+#     if yntacik % 2 == 0:
+#      print(yntacik)
+
+# number = int(input("Please enter some number: "))
+# result = 0
+#
+# print(f"Sum from 1 to {number} is:", end="")
+# while number > 0:
+#     result += number
+#     number -= 1
+
+
